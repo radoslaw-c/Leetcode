9dd3cc79d27d32f819f76cb81072dd3f01526999
Radek <radoslaw.c@o2.pl>
2024-02-20 18:44:24 +0100

    Working solution for majorityElement

--- Leetcode/Leetcode/Leetcode.cpp
+++ Leetcode/Leetcode/Leetcode.cpp
@@ -1,6 +1,24 @@
 #include <iostream>
+#include <vector>
+
+class Solution
+{
+public:
+    int majorityElement(std::vector<int>& nums)
+    {
+        auto minOccur = static_cast<float>(nums.size()) / 2;
+
+        for (auto& num : nums)
+        {
+            if (std::count(nums.begin(), nums.end(), num) >= minOccur)
+                return num;
+        }
+    }
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    std::vector<int> sampleVector = { 3, 2, 3 };
+    auto solution = Solution();
+    solution.majorityElement(sampleVector);
 }
