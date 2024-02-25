13842d5366056544e3ca3b8aaa69bb3d41ea2d47
Radek <radoslaw.c@o2.pl>
2024-02-25 12:06:01 +0100

    Solution implemented
    Quite nicely time optimized.
    time: O(n)space: O(1)

--- Leetcode/Leetcode/Leetcode.cpp
+++ Leetcode/Leetcode/Leetcode.cpp
@@ -1,6 +1,44 @@
 #include <iostream>
+#include <vector>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& numbers, int target) {
+        auto pt1 = 0;
+        auto pt2 = numbers.size() - 1;
+
+        vector<int> idx;
+        idx.resize(2);
+
+        while (pt1 < pt2)
+        {
+            auto sum = numbers[pt1] + numbers[pt2];
+            if (sum == target)
+                break;
+
+            if (sum > target)
+            {
+                --pt2;
+                continue;
+            }
+            else
+            {
+                ++pt1;
+                continue;
+            }
+        }
+        idx[0] = pt1 + 1;
+        idx[1] = pt2 + 1;
+
+        return idx;
+    }
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+
+
+    
 }
