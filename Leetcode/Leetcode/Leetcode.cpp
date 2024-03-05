3d135c4904fb9c6ff76271b6d0419c259880a8af
Radek <radoslaw.c@o2.pl>
2024-03-05 19:13:06 +0100

    Implementation

--- Leetcode/Leetcode/Leetcode.cpp
+++ Leetcode/Leetcode/Leetcode.cpp
@@ -1,6 +1,35 @@
 #include <iostream>
+#include <vector>
+#include <unordered_map>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> diffs;
+
+        for (auto i = 0; i < nums.size(); ++i)
+        {
+            diffs[target - nums[i]] = i;
+        }
+
+        for (auto i = 0; i < nums.size(); ++i)
+        {
+            auto num = nums[i];
+            auto it = diffs.find(num);
+            if (it != diffs.end() && (*it).second != i)
+            {
+                return { (*it).second, i };
+            }
+        }
+        return {};
+    }
+};
 
 int main()
 {
-    std::cout << "Hello World!\n";
+    Solution sol;
+    vector<int> vec{ 3, 2, 4 };
+    auto ans = sol.twoSum(vec, 6);
 }
